a4c3a4982bc40890dcd28f07cbdb3ad36a019363
zoohang <136927705@qq.com>
2018-11-02 00:20:44 +0800

    遗漏文件追缴

---
+++ app/admin/validate/CategoryValidate.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\admin\validate;
+
+use app\admin\model\RouteModel;
+use think\Validate;
+
+class CategoryValidate extends Validate
+{
+    protected $rule = [
+        'name'  => 'require',
+    ];
+    protected $message = [
+        'name.require' => '分类名称不能为空',
+    ];
+
+    protected $scene = [
+//        'add'  => ['user_login,user_pass,user_email'],
+//        'edit' => ['user_login,user_email'],
+    ];
+
+}
