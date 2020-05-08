366bcde91a21a0838c10240e78a60cfd7574c4c1
yuan <136927705@qq.com>
2020-05-08 23:19:50 +0800

    不能修改common中的cmf_get_option方法, 容易出问题

--- simplewind/cmf/common.php
+++ simplewind/cmf/common.php
@@ -501,7 +501,8 @@ function cmf_get_option($key)
     }
 
     $optionValue = cache('cmf_options_' . $key);
-    if ($optionValue!==false) {
+
+    if (empty($optionValue)) {
         $optionValue = Db::name('option')->where('option_name', $key)->value('option_value');
         if (!empty($optionValue)) {
             $optionValue = json_decode($optionValue, true);
