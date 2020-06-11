e8ef8c6e53f2bad1d3aba52406dc6b3bd737a789
yuanzuohang <136927705@qq.com>
2020-06-11 18:06:52 +0800

    更新了后台的一些bug

--- api/route.php
+++ api/route.php
@@ -6,6 +6,20 @@
 // +----------------------------------------------------------------------
 // | Author: Dean <zxxjjforever@163.com>
 // +----------------------------------------------------------------------
+use think\Route;
+//自定义路由
+Route::group('v2', [
+    'course/videoMain' => ['@/v2/Index/courseVideoMain'],
+    'course/videoList' => ['@/v2/Index/videoList'],
+    'mall/index' => ['@/v2/Index/mallIndex'],
+    'mall/getList' => ['@/v2/Index/getList'],
+]);
+Route::group('v1', [
+    'exam/getCategoryExam' => ['@/v2/Index/examGetCategoryExam'],
+    'exam/getSchool' => ['@/v2/Index/examGetSchool'],
+    'exam/getExamList' => ['@/v2/Index/getExamList'],
+]);
+
 $apps = cmf_scan_dir(APP_PATH . '*', GLOB_ONLYDIR);
 
 foreach ($apps as $app) {
--- api/v2/controller/IndexController.php
+++ api/v2/controller/IndexController.php
@@ -9,16 +9,19 @@
 namespace api\v2\controller;
 
 use api\v1\model\FeedbackModel;
+use api\v1\model\OrderModel;
+use api\v2\model\CategoryModel;
 use api\v2\model\CourseModel;
 use api\v1\model\DakaModel;
 use api\v1\model\ExamModel;
 use api\v1\model\PortalPostModel;
 use api\v1\model\SlideItemModel;
 use api\v2\model\MallModel;
-use cmf\controller\RestUserBaseController;
+use app\admin\model\GoodsModel;
+use cmf\controller\RestBaseController;
 use think\Db;
 
-class IndexController extends \api\v1\controller\IndexController
+class IndexController extends RestBaseController
 {
     protected $expire = 600;
     // 首页信息
@@ -135,5 +138,173 @@ class IndexController extends \api\v1\controller\IndexController
         } else {
             $this->error('提交失败！');
         }
+    }
+
+    public function courseVideoMain() {
+        $slide = SlideItemModel::instance()->getOne(5);//id=5
+        //初始化内容 获取分类
+        $category = CategoryModel::instance(3)->getFirstLevelCategory();
+        //获取全部的内容 列表
+        $course_types = CourseModel::instance()->getCourseTypeList();
+        $level = CourseModel::$levels;
+        $list = $this->videoList(0,0,0,15);
+        $this->success('ok', compact('slide', 'category', 'course_types', 'level', 'list'));
+    }
+
+    //视频列表
+    public function videoList($cid=0, $course_type=0, $level='',$limit=10) {
+        $cid = $cid ?: $this->request->param('cid', 0, 'intval,abs,trim');
+        $course_type = $course_type ?: $this->request->param('course_type', 0, 'intval,abs,trim');
+        $cid = trim($cid,',');
+        $course_type = trim($course_type,',');
+        $level = $level ?: $this->request->param('level');
+        $list = [];
+        $where = ['type'=>1];
+        if ($cid) {
+            $where['pid'] = ['in', explode(',', $cid)];
+        }
+        if ($course_type) {
+            $where['course_type'] = ['in', explode(',', $course_type)];
+        }
+        if (!in_array($level, [null, '', 0])) $where['level'] = $level;
+        $list = CourseModel::instance()
+            ->field(CourseModel::$list_field)
+            ->where($where)
+            ->order(['list_order'=>'asc','cid'=>'desc'])
+            ->cache(true, 60)
+            ->paginate($limit)->toArray();
+        foreach ($list['data'] as &$item) {
+            $item['thumbnail_200'] = get_image_url($item['thumbnail'], 200);
+            $item['thumbnail_480'] = get_image_url($item['thumbnail'], 480);
+            unset($item['thumbnail']);
+        }
+        unset($item);
+        if ($this->request->action() != strtolower(__FUNCTION__)) {
+            return $list['data'];
+        } else {
+            $this->success('ok', $list);
+        }
+    }
+
+    public function mallIndex() {
+        //轮播图
+        $slide = SlideItemModel::instance()->getOne(4);//id=4
+        //分类列表
+        $field = ['id', 'parent_id', 'name'];
+        $categorys = CategoryModel::instance(MallModel::$ctype)->getFirstLevelCategory();
+        //初始化的商品列表
+        $list = $this->getList(0,0,15);
+        $data = [
+            'slide'=>$slide,
+            'categorys'=>$categorys,
+            'mall_types'=> MallModel::instance()->getMallTypeList(),
+            'list'=>$list,
+        ];
+        $this->success('ok', $data);
+    }
+
+    // 筛选商品列表
+    public function getList($cid=0, $mall_type=0, $limit=10) {
+        $cid = $cid ?: $this->request->param('cid', 0, 'intval,abs,trim');
+        $mall_type = $mall_type ?: $this->request->param('mall_type', 0, 'intval,abs,trim');
+        $cid = trim($cid,',');
+        $mall_type = trim($mall_type,',');
+        $model = new MallModel;
+        $where = [];
+        if ($cid != 0) {
+            $where['cid'] = ['IN', explode(',', trim($cid, ','))];
+        }
+        if ($mall_type != 0) {
+            $where['mall_type'] = ['IN', explode(',', trim($mall_type, ','))];
+        }
+        $list = $model
+            ->field(MallModel::$list_field)
+            ->where($where)
+            ->order(['list_order'=>'asc', 'published_time'=>'desc'])
+            ->cache(true, 60)
+            ->paginate($limit)->toArray();
+        foreach ($list['data'] as &$item) {
+            $item['thumbnail_200'] = get_image_url($item['thumbnail'], 200);
+            $item['thumbnail_480'] = get_image_url($item['thumbnail'], 480);
+            unset($item['thumbnail']);
+        }
+        if ($this->request->action() != strtolower(__FUNCTION__)) {
+            return $list['data'];
+        } else {
+            $this->success('ok', $list);
+        }
+    }
+
+    public function examGetCategoryExam() {
+        $where = ['a.status'=>1, 'a.type'=>1];
+        $list = DB::name('category a')
+            ->join('__EXAM_SCHOOL_RELATION__ b', 'a.id=b.category_id')
+            ->field('a.*')
+            ->where($where)
+            ->group('b.category_id')
+            ->select()
+            ->toArray();
+        $this->success('ok', $list);
+    }
+
+    public function examGetSchool() {
+        $category_id = $this->request->param('category_id', 0, 'intval,abs');
+        $where = ['b.status'=>1];
+        if ($category_id) $where['a.category_id'] = $category_id;
+        $list = DB::name('exam_school_relation a')
+            ->join('__SCHOOL__ b', 'a.school_id=b.id')
+            ->join('__EXAM__ c', 'a.exam_id=c.id and c.`status`=1')
+            ->distinct('b.id')
+            ->field('b.*')
+            ->where($where)
+            ->order(['b.list_order'=>'asc','b.id'=>'asc'])
+            ->select()
+            ->toArray();
+        $this->success('ok', $list);
+    }
+
+    public function getExamList() {
+        $category_id = $this->request->param('category_id', 0, 'intval,abs');
+        $school_id = $this->request->param('school_id', 0, 'intval,abs');
+        $limit = 10;
+        $where = ['b.status'=>1, 'a.school_id'=>$school_id];
+        if ($category_id) $where['a.category_id'] = $category_id;
+        $order = ['list_order'=>'asc', 'id'=>'desc'];
+        $list = DB::name('exam_school_relation a')->join('__EXAM__ b', 'a.exam_id=b.id')
+            ->field('b.*')
+            ->where($where)
+            ->order($order)
+            ->paginate($limit)
+            ->toArray();
+        //价格
+        $goods_ids = array_column($list['data'], 'goods_id');
+        $where = ['goods_id'=>['in', $goods_ids]];
+        $goods = GoodsModel::instance()->where($where)->field('goods_id,price,cost_price,stock')->select();
+
+        //是否已经购买
+        if ($this->userId) {
+            $order = OrderModel::instance()->where($where)->where(['user_id'=>$this->userId, 'pay_status'=>2])->field('goods_id,order_id')->select();
+        } else {
+            $order = [];
+        }
+        foreach ($list['data'] as &$item) {
+            foreach ($goods as $g) {
+                if ($item['goods_id'] == $g['goods_id']) {
+                    $item['price'] = $g['price'];
+                    $item['cost_price'] = $g['cost_price'];
+                    $item['stock'] = $g['stock'];
+                    continue;
+                }
+            }
+            $item['is_buy'] = 0;
+            foreach ($order as $o) {
+                if ($item['goods_id'] == $o['goods_id']) {
+                    $item['is_buy'] = 1;
+                    continue;
+                }
+            }
+        }
+        unset($item);
+        $this->success('ok', $list);
     }
 }
